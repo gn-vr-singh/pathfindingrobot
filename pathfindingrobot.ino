b1b6a21871bf203ff78e180030a8d8b93df611d5
gn-vr-singh <154sainigan@gmail.com>
2024-07-30 23:26:27 -0400

    Add files via upload

---
+++ pathfindingrobot.ino
@@ -0,0 +1,110 @@
+// hey there is code in here!!!
+#include <tcs3200.h>
+#define COLOR_0 A0
+#define COLOR_1 A1
+#define COLOR_2 A2
+#define COLOR_3 A3
+#define COLOR_OUT A4
+
+#define IR_1 4
+#define IR_2 7
+#define IR_3 8
+#define IR_4 12
+
+#define MOTOR_PIN1 3
+#define MOTOR_PIN2 5
+#define MOTOR_PIN3 6
+#define MOTOR_PIN4 9
+
+#define TRIG_PIN 10
+#define ECHO_PIN 11
+
+
+unsigned long colorSensorMillis = 0;
+unsigned long irSensorMillis = 0; // Timer to track the last report of the IR sensors
+unsigned long ultrasonicSensorMillis = 0;
+// Get the current run time in milliseconds
+unsigned long currentMillis;
+
+// State Logic
+String currentState = "Null";
+String oldCurrentState = "Null";
+
+String turnDirection = "";
+String lastTurnDirection = "";
+bool isTurning = false;
+bool wallDetected = false;
+
+int currentDistance = 0;
+
+
+// Test motor control
+void motorTest() {
+  motorControl(255, 255);
+  delay(1000);
+  motorControl(0, 0);
+  delay(100);
+  motorControl(255, -255);
+  delay(500);
+  motorControl(0,0);
+  delay(100);
+  Serial.println("Motor code is running!");
+}
+
+void setup() {
+// put your setup code here, to run once:
+
+// Setup Infrared Pins
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(IR_3, INPUT);
+  pinMode(IR_4, INPUT);
+
+  // Setup Motor Pins
+  pinMode(MOTOR_PIN1, OUTPUT);
+  pinMode(MOTOR_PIN2, OUTPUT);
+  pinMode(MOTOR_PIN3, OUTPUT);
+  pinMode(MOTOR_PIN4, OUTPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Setup onboard RGB LED pins
+  /*
+  WifiDrv::pinMode(LED_R, OUTPUT);
+  WifiDrv::pinMode(LED_G, OUTPUT);
+  WifiDrv::pinMode(LED_B, OUTPUT);
+  */
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Get the current run time in milliseconds
+  currentMillis = millis();
+
+  // Check the states of the IR sensors every 500ms
+  if (currentMillis - irSensorMillis >=20) {
+    irSensorMillis = currentMillis;
+    readInfrared();
+  }
+
+  // Read the color sensor
+  if (currentMillis - colorSensorMillis >=20) {
+    colorSensorMillis = currentMillis;
+    readColorSensor();
+  }
+
+  // Read the infrared sensor
+
+   if (currentMillis - ultrasonicSensorMillis >=20) {
+    ultrasonicSensorMillis = currentMillis;
+    readUltrasonicSensor();
+  }
+
+robotLogic();
+  
+}
---
+++ readInfrared.ino
@@ -0,0 +1,21 @@
+int* readInfrared() {
+  static int IRvalues[4]; // Read analog values from infrared sensors
+  IRvalues[0] = digitalRead(IR_1);
+  IRvalues[1] = digitalRead(IR_2);
+  IRvalues[2]  = digitalRead(IR_3);
+  IRvalues[3]  = digitalRead(IR_4);
+
+  // Print the read values to the serial monitor
+  /*
+  Serial.print("IR_1: ");
+  Serial.print(IRvalues[0]);
+  Serial.print(", IR_2: ");
+  Serial.print(IRvalues[1]);
+  Serial.print(", IR_3: ");
+  Serial.print(IRvalues[2]);
+  Serial.print(", IR_4: ");
+  Serial.println(IRvalues[3]);
+  */
+
+  return IRvalues; // Return the array containing the IR sensor values
+}
